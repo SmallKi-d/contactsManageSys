c184bf00acabd4108aec3e508f7e3e101dec5f4a
SmallKi-d <1502598781@qq.com>
2024-01-06 17:30:01 +0800

    Update CMAKE_CXX_STANDARD

--- source/save.cpp
+++ source/save.cpp
@@ -28,30 +28,30 @@ void Save::SaveToFile(const std::string &filename) {
 }
 
 void Save::LoadFromFile(const std::string &filename, list *addressList) {
-//    ifstream inFile(filename);
-//    string line;
-//
-//    // 判断是否能打开文件
-//    if (!inFile.is_open()) {
-//        std::cerr << "无法打开文件" << std::endl;
-//        return;
-//    }
-//
-//    // 循环读文件的每一行
-//    while (getline(inFile, line)) {
-//        std::istringstream iss(line);
-//        // 拆分整行字符串
-//        if (!(iss >> addressList -> l_Arr[addressList->l_size].c_name)) {
-//            break;
-//        }
-//
-//    }
-//
-//    inFile.close();
+    ifstream inFile(filename);
+    string line;
+
+    // 判断是否能打开文件
+    if (!inFile.is_open()) {
+        std::cerr << "无法打开文件" << std::endl;
+        return;
+    }
+
+    // 循环读文件的每一行
+    while (getline(inFile, line)) {
+        std::istringstream iss(line);
+        // 拆分整行字符串
+        if (!(iss >> addressList -> l_Arr[addressList->l_size].c_name)) {
+            break;
+        }
+
+    }
+
+    inFile.close();
 }
 
 void Save::ifFileExist(const std::string &filename) {
-    bool exists = __fs::filesystem::exists(filename);
+    bool exists = filesystem::exists(filename);
     if (!exists){
         cerr << "文件不存在, 正在生成通讯录文件..." << endl;
         // 等待 3s ，防止程序跑得过快
